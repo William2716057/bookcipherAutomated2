dbe6fcebd281322be0b34f480b450fb60154545a
User <Willspri@gmail.com>
2024-07-10 12:06:49 +1200

    comments

--- bookCipherAutomated2.py
+++ bookCipherAutomated2.py
@@ -1,12 +1,13 @@
 import random
 
+#input prompts
 def get_valid_input(prompt):
     while True:
         user_input = input(prompt)
         if len(user_input) > 0:
             return user_input
         print("Please enter at least one character.")
-
+#search for characters
 def find_char(text, characters):
     results = {char: [] for char in characters}
     lines = text.split('\n')
@@ -20,16 +21,16 @@ def find_char(text, characters):
 
 plaintext = get_valid_input("Enter characters: ")
 
-
+#read from file
 with open("sample.txt", "r") as file:
     text = file.read()
 
 positions = find_char(text, plaintext)
 
 
-
+#initialise output
 output = ""
-
+#get random character positions
 for char, pos_list in positions.items():
     if pos_list:
         random_position = random.choice(pos_list)
